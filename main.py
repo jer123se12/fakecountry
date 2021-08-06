22734f7453d7457e7e5baaf51a2ebde5743a299f
jer123se12 <64419693+jer123se12@users.noreply.github.com>
2021-08-06 14:05:18 +0800

    ignore config file

--- main.py
+++ main.py
@@ -22,12 +22,25 @@ async def on_message(message):
     name="ping",
     help = "Sends the ping of the bot back"
     )
-async def pingReply(ctx):
+async def pingReply(ctx, *args):
     timeSent = msg.created_at.strftime("%S.%f")
     timeNow = datetime.datetime.utcnow().strftime("%S.%f")
     timeDiff = float(timeNow) - float(timeSent)
     response  = math.floor(timeDiff*1000000)/1000
     response = "Ping: **" + str(response) + "ms" +"**"
     await ctx.channel.send(response)
+@bot.command(
+    name="sort",
+    help = "sorts an array seperated by ','"
+    )
+async def sorting(ctx, message):
+    message=message.split(',')
+    l=[]
+    for i in message:
+        if i.isdigit():
+            l.append(int(i))
+    await ctx.channel.send(','.join([str(a) for a in sorted(l)]))
+
+
 
 bot.run(TOKEN)
